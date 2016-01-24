c01043ec1db20e20c72613ca5ce763102fcf2606
cnexus <cnexus0@gmail.com>
2016-01-23 19:56:53 -0500

    Add support for toggling recording during AR mode

--- app/src/main/java/at/maui/cardar/ui/activity/ArActivity.java
+++ app/src/main/java/at/maui/cardar/ui/activity/ArActivity.java
@@ -1,8 +1,6 @@
 package at.maui.cardar.ui.activity;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.os.Vibrator;
 
 import com.google.vrtoolkit.cardboard.CardboardActivity;
 import com.google.vrtoolkit.cardboard.CardboardView;
@@ -12,13 +10,11 @@ import at.maui.cardar.ui.ar.Renderer;
 import at.maui.cardar.ui.widget.CardboardOverlayView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import timber.log.Timber;
 
 /**
  * Created by maui on 02.07.2014.
  */
 public class ArActivity extends CardboardActivity {
-
     @InjectView(R.id.overlay)
     CardboardOverlayView overlayView;
 
@@ -26,9 +22,8 @@ public class ArActivity extends CardboardActivity {
     CardboardView cardboardView;
 
     private Renderer mRenderer;
-
-    private Vibrator mVibrator;
-    private int mScore = 0;
+    private Recorder mRecorder;
+    private int mRecordingNum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +33,6 @@ public class ArActivity extends CardboardActivity {
 
         // Inject views
         ButterKnife.inject(this);
-
-        mVibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
         // Associate a CardboardView.StereoRenderer with cardboardView.
         mRenderer = new Renderer(this);
@@ -53,16 +46,13 @@ public class ArActivity extends CardboardActivity {
 
     @Override
     public void onCardboardTrigger() {
-        Timber.i("onCardboardTrigger");
+        // Toggle sound recording
+        mRecordingNum++;
 
-//        if (mRenderer.isLookingAtObject()) {
-//            mScore++;
-//            overlayView.show3DToast("Found it! Look around for another one.\nScore = " + mScore);
-//            mRenderer.hideObject();
-//        } else {
-//            overlayView.show3DToast("Look around to find the object!");
-//        }
-        // Always give user feedback
-        //mVibrator.vibrate(50);
+        if(mRecordingNum % 2 == 0){ // Recording was stopped
+            mRecorder.startRecording();
+        }else{ // Recording was started
+            mRecorder.stopRecording();
+        }
     }
 }
---
+++ app/src/main/java/at/maui/cardar/ui/activity/Recorder.java
@@ -0,0 +1,9 @@
+package at.maui.cardar.ui.activity;
+
+/**
+ * Created by Carlos on 1/23/2016.
+ */
+public interface Recorder {
+    public boolean startRecording();
+    public boolean stopRecording();
+}
